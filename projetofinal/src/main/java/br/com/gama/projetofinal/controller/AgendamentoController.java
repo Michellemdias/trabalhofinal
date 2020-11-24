bbf69e929c03d572c70b7c012ef5de0c3db07d24
lucas menezes <lucas-rdm@hotmail.com>
2020-11-24 12:08:47 -0300

    backend

---
+++ projetofinal/src/main/java/br/com/gama/projetofinal/controller/AgendamentoController.java
@@ -0,0 +1,90 @@
+package br.com.gama.projetofinal.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.gama.projetofinal.dao.AgendamentoDAO;
+import br.com.gama.projetofinal.model.Agendamento;
+
+@RestController
+@CrossOrigin("*")
+public class AgendamentoController {
+	
+	@Autowired
+	private AgendamentoDAO dao;
+	
+	@PostMapping("/relatorioporagencia")
+	public ResponseEntity<List<Agendamento>> getAgendamentoAgencia(@RequestBody Agendamento objeto){
+		List<Agendamento> lista = dao.findByAgencia(objeto.getAgencia().getId());
+		if(lista.size()==0) return ResponseEntity.status(404).build();
+		return ResponseEntity.ok(lista);
+		
+	}
+	
+	@PostMapping("/relatoriopordata")
+	public ResponseEntity<List<Agendamento>> getAgendamentoData(@RequestBody Agendamento objeto){
+		List<Agendamento> lista = dao.findByDataagendamento(objeto.getDataagendamento());
+		if(lista.size()==0) return ResponseEntity.status(404).build();
+		return ResponseEntity.ok(lista);
+		
+	}
+	
+	@PostMapping("/relatorioporcliente")
+	public ResponseEntity<List<Agendamento>> getAgendamentoCliente(@RequestBody Agendamento objeto){
+		List<Agendamento> lista = dao.findByNomecli(objeto.getNomecli());
+		if(lista.size()==0) return ResponseEntity.status(404).build();
+		return ResponseEntity.ok(lista);
+	}
+	
+	@PostMapping("/relatorioporagenciadata")
+	public ResponseEntity<List<Agendamento>> getAgendamentoAgenciaData(@RequestBody Agendamento objeto){
+		List<Agendamento> lista = dao.findByAgenciaAndDataagendamento(objeto.getAgencia().getId(),objeto.getDataagendamento());
+		if(lista.size()==0) return ResponseEntity.status(404).build();
+		return ResponseEntity.ok(lista);
+		
+	}
+	
+	@PostMapping("/relatorioporagenciacliente")
+	public ResponseEntity<List<Agendamento>> getAgendamentoAgenciaCliente(@RequestBody Agendamento objeto){
+		List<Agendamento> lista = dao.findByAgenciaAndNomecli(objeto.getAgencia().getId(),objeto.getNomecli());
+		if(lista.size()==0) return ResponseEntity.status(404).build();
+		return ResponseEntity.ok(lista);
+		
+	}
+	
+	@PostMapping("/relatorioporclientedata")
+	public ResponseEntity<List<Agendamento>> getAgendamentoClienteData(@RequestBody Agendamento objeto){
+		List<Agendamento> lista = dao.findByNomecliAndDataagendamento(objeto.getNomecli(),objeto.getDataagendamento());
+		if(lista.size()==0) return ResponseEntity.status(404).build();
+		return ResponseEntity.ok(lista);
+		
+	}
+	
+	@PostMapping("/relatorioporagenciaclientedata")
+	public ResponseEntity<List<Agendamento>> getAgendamentoAgenciaClienteData(@RequestBody Agendamento objeto){
+		List<Agendamento> lista = dao.findByAgenciaAndNomecliAndDataagendamento(objeto.getAgencia().getId(),objeto.getNomecli(),objeto.getDataagendamento());
+		if(lista.size()==0) return ResponseEntity.status(404).build();
+		return ResponseEntity.ok(lista);
+		
+	}
+	
+	@PostMapping("/novoagendamento")
+	public ResponseEntity<Agendamento> gravar(@RequestBody Agendamento objeto){
+		try {
+			dao.save(objeto);
+			return ResponseEntity.ok(objeto);
+		} catch(Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.status(403).build();
+		}
+	}
+
+}
---
+++ projetofinal/src/main/java/br/com/gama/projetofinal/dao/GerenteDAO.java
@@ -0,0 +1,22 @@
+package br.com.gama.projetofinal.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import br.com.gama.projetofinal.model.Gerente;
+
+/*
+ * DAO => Data Access Object
+ * Classes que contém os métodos que irão manipular os dados
+ * manipular os dados = CRUD
+ * C => Create (inserir dado na tabela)
+ * R => Read (consultar dado na tabela)
+ * U => Update (alterar dado na tabela)
+ * D => Delete (apagar dado na tabela)
+ * Classe CrudRepository<1ºClasseBeans,2ºTipodeDadoPK>
+ */
+
+public interface GerenteDAO extends CrudRepository<Gerente,Integer>{
+	
+	public Gerente findByEmailAndSenha(String email, String senha);
+
+}
