1c5430b0d16b1a7109caec9425db21bacca421ae
Michelle Dias <michellemdias@yahoo.com.br>
2020-11-24 11:53:20 -0300

    xxxx

---
+++ agendamentocliente.js
@@ -0,0 +1,14 @@
+function preencheragencias(lista){
+    var saida="";
+    for (cont=0;cont<lista.length;cont++){
+        saida+=
+        "<option value='"+lista[cont].id+"'>" + lista[cont].nomeArtistico + "</option>";
+    }
+    document.getElementById("inputAg").innerHTML=saida;
+}
+
+function carregaragencias(){
+    fetch("http://localhost:8080/agencias")
+    .then(res=> res.json())
+    .then(res=> preencheragencias(res));
+}
---
+++ logingerente.js
@@ -0,0 +1,38 @@
+function carregarusuario() {
+    var usuariologado = localStorage.getItem("logado");
+    if (usuariologado == null) {
+        window.location = "login.html";
+    } else {
+        var usuariojson = JSON.parse(usuariologado);
+        document.getElementById("foto").innerHTML =
+            "<img width='25%' heigth='25%' alt='Foto não encontrada'src=imagens/" + usuariojson.foto + ">";
+        document.getElementById("dados").innerHTML =
+            "<h3>" + usuariojson.nome + "<br>" + usuariojson.email + "<br>ID:" + usuariojson.id + "<br> </h3>"
+    }
+}
+
+function logar() {
+    var objeto = {
+        "email": document.getElementById("txtemail").value,
+        "senha": document.getElementById("txtsenha").value
+    }
+
+
+    var cabecalho = {
+        method: "POST",
+        body: JSON.stringify(objeto),
+        headers: {
+            "Content-type": "application/json"
+        }
+    }
+
+    fetch("http://localhost:8080/login", cabecalho)
+        .then(res => res.json())
+        .then(res => {
+            localStorage.setItem("logado", JSON.stringify(res));
+            window.location = "usuario.html";
+        })
+        .catch(err => {
+            window.alert("Ocorreu um erro");
+        });
+}
